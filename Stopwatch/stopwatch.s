548865387f90c58524bf8815a986c0695abd7f41
Simline Gijo <73033366+sgijo@users.noreply.github.com>
2024-09-04 17:03:59 -0400

    Update stopwatch.s
    made stopwatch format...more work to do

--- Stopwatch/stopwatch.s
+++ Stopwatch/stopwatch.s
@@ -6,29 +6,77 @@
 @Deliverable 2: your code on github and in your writeup.  We will check in class and look at accuracy as well.
 
 	.text
-	.global _start
+        .global main
+        .func main
+
+main:
+        PUSH {LR}
+        BAL print
+        MOV R3, #0
 
 _start:	
 
-	mov	r2, #15		@ load 15 into r2 (outer loop count)
+	mov	r2, #120		@ load 18 into r2 (outer loop count)
+	MOV 	R3, #0
 
-l1:	ldr	r1, =1000000000	@ r1 = 1,000,000,000 (inner loop count) 
+l1:	ldr	r1, =10000000	@ r1 = 1,000,000,0 (inner loop count) 
 l2:	subs	r1, r1, #1		@ r1 = r1 – 1, decrement r1 (inner loop) 
 	bne	l2			@ repeat it until r1 = 0 
+	b	print
 	subs	r2, r2, #1		@ r2 = r2 – 1, decrement r2 (outer loop) 
 	@bne	l1			@ repeat it until r2 = 0 
 
-print:     mov     R0, #1          @ 1 = StdOut
-           ldr     R1, =counter @sting to print
-           mov     R2, #4         @length of out string
-           mov     R7, #4          @linux write system call
-           svc     0               @call linux to print
-           b       l1
+print:    
 
-	@ terminate the program
-	mov   	r7, #1
-	svc   0
+	LDR 	R0, =string         @ Obtain the String Format Location in R0
+        LDR 	R1, =iterations		
+        LDR 	R1, [R1]            @ Obtain the Number in Iterations into R1
+        BL 	printf
+	
+	LDR 	R0, =string         @ Obtain the String Format Location in R0
+        LDR 	R1, =iterations		
+        LDR 	R1, [R1, #4]            @ Obtain the Number in Iterations into R1
+        BL 	printf
+
+	LDR 	R0, =colon         @ Obtain the String Format Location in R0
+        BL 	printf
+
+	LDR 	R0, =string         @ Obtain the String Format Location in R0
+        LDR 	R1, =iterations		
+        LDR 	R1, [R1, #8]            @ Obtain the Number in Iterations into R1
+        BL 	printf
+	
+	LDR 	R0, =string         @ Obtain the String Format Location in R0
+        LDR 	R1, =iterations		
+        LDR 	R1, [R1, #12]            @ Obtain the Number in Iterations into R1
+        BL 	printf
+	
+	LDR 	R0, =colon         @ Obtain the String Format Location in R0
+        BL 	printf
+	
+	LDR 	R0, =string         @ Obtain the String Format Location in R0
+        LDR 	R1, =iterations		
+        LDR 	R1, [R1, #16]            @ Obtain the Number in Iterations into R1
+        BL 	printf
+	
+	LDR 	R0, =string         @ Obtain the String Format Location in R0
+        LDR 	R1, =iterations		
+        LDR 	R1, [R1, #20]            @ Obtain the Number in Iterations into R1
+        BL 	printf
+
+	LDR 	R0, =newline         @ Obtain the String Format Location in R0
+        BL 	printf
+	
+_exit:
+        POP {PC}
+        MOV PC, LR
 
 .data
-@printing out group fours message to the world
-counter: .ascii "123\n"
+string:
+        .asciz "%d"		@ Printable Number Format
+colon:
+	.asciz ":"
+newline:
+	.asciz "\n"
+iterations:
+        .word 0, 1, 2, 3, 4, 5  @ remaining loop iterations MM:SS:HH
