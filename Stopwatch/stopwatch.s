aebb2022c7bec82ae9b2a62030b9ff301c46cc36
Simline Gijo <73033366+sgijo@users.noreply.github.com>
2024-09-04 18:01:27 -0400

    final code before commenting for stopwatch.s

--- Stopwatch/stopwatch.s
+++ Stopwatch/stopwatch.s
@@ -12,19 +12,94 @@
 main:
         PUSH {LR}
         BAL print
-        MOV R3, #0
 
 _start:	
 
-	mov	r2, #120		@ load 18 into r2 (outer loop count)
-	MOV 	R3, #0
+	@mov	r2, #120		@ load 18 into r2 (outer loop count)
+	LDR 	R2, =iterations
+	MOV 	R1, #0
+	STR 	R1, [R2, #4]
 
-l1:	ldr	r1, =10000000	@ r1 = 1,000,000,0 (inner loop count) 
+l1:	ldr	r1, =8900000	@ r1 = 8,700,000 (inner loop count) 
 l2:	subs	r1, r1, #1		@ r1 = r1 – 1, decrement r1 (inner loop) 
 	bne	l2			@ repeat it until r1 = 0 
-	b	print
-	subs	r2, r2, #1		@ r2 = r2 – 1, decrement r2 (outer loop) 
+	b 	_h1
+	@subs	r2, r2, #1		@ r2 = r2 – 1, decrement r2 (outer loop) 
 	@bne	l1			@ repeat it until r2 = 0 
+
+_h1:
+        LDR R1, =iterations
+        LDR R1, [R1, #20]
+        ADDS R1, #0x1			@ Iterate the Count
+        LDR R2, =iterations
+        STR R1, [R2, #20]            @ Stores the value in R1 into the memory address in R2
+        
+	CMP R1, #10
+        BEQ _h2
+        
+        b 	print
+
+
+_h2:
+	MOV R1, #0
+	STR R1, [R2, #20]
+	
+	LDR R1, =iterations
+        LDR R1, [R1, #16]
+        ADDS R1, #0x1			@ Iterate the Count
+        LDR R2, =iterations
+        STR R1, [R2, #16]            @ Stores the value in R1 into the memory address in R2
+        
+	CMP R1, #10
+        BEQ _s1
+	
+	B	print
+
+_s1:
+
+	MOV R1, #0
+	STR R1, [R2, #16]
+	
+	LDR R1, =iterations
+        LDR R1, [R1, #12]
+        ADDS R1, #0x1			@ Iterate the Count
+        LDR R2, =iterations
+        STR R1, [R2, #12]            @ Stores the value in R1 into the memory address in R2
+        
+	CMP R1, #10
+        BEQ _s2
+	
+	B	print
+	
+_s2:
+	MOV R1, #0
+	STR R1, [R2, #12]
+	
+	LDR R1, =iterations
+        LDR R1, [R1, #8]
+        ADDS R1, #0x1			@ Iterate the Count
+        LDR R2, =iterations
+        STR R1, [R2, #8]            @ Stores the value in R1 into the memory address in R2
+        
+	CMP R1, #6
+        BEQ _m1
+	
+	B	print
+	
+_m1:
+	MOV R1, #0
+	STR R1, [R2, #8]
+	
+	LDR R1, =iterations
+        LDR R1, [R1, #4]
+        ADDS R1, #0x1			@ Iterate the Count
+        LDR R2, =iterations
+        STR R1, [R2, #4]            @ Stores the value in R1 into the memory address in R2
+        
+	CMP R1, #2
+        BEQ _start
+	
+	B	print
 
 print:    
 
@@ -67,6 +142,8 @@ print:
 	LDR 	R0, =newline         @ Obtain the String Format Location in R0
         BL 	printf
 	
+	b 	l1
+	
 _exit:
         POP {PC}
         MOV PC, LR
@@ -79,4 +156,4 @@ colon:
 newline:
 	.asciz "\n"
 iterations:
-        .word 0, 1, 2, 3, 4, 5  @ remaining loop iterations MM:SS:HH
+        .word 0, 0, 0, 0, 0, 0  @ remaining loop iterations MM:SS:HH
