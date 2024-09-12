7ad1c1caa05b79779edcf5310da75c3497813701
Simline Gijo <73033366+sgijo@users.noreply.github.com>
2024-09-12 11:18:57 -0400

    Update BlinkLibrarySW.c

--- BlinkLibrarySW.c
+++ BlinkLibrarySW.c
@@ -1,23 +1,21 @@
-#include <E4235.h>
+//#include <E4235.h>
 #include <stdio.h>
+
+extern void E4235_Write(int, int);
+extern void E4235_Delaynano(int);
 
 int main(int argc, char **argv)
 {
-	//int test[0];
-	//test[0] = 12;
-	int test = 12;
-	int *ptr = (int*)test;
+	// 60 Hz is 7,450,000
+	int delayTime;
+	delayTime = 1;
 	
-	while (1) {
-		printf("Iterate\n");
-		E4235_multiwrite(ptr, 1, 1);
-		//4235_Write(12, 1);
-		E4235_delayNano(100);
-		//printf("go dawgs!\n");
-		//E4235_multiwrite(test[0], 1, 0);
+	while(1) {
+		
+		E4235_Write(12, 1);
+		E4235_Delaynano(delayTime);
 		E4235_Write(12, 0);
-		//E4235_delayNano(100);
-		//printf("sic' em\n");
-		//return 0;
+		E4235_Delaynano(delayTime);
+		
 	}
 }
