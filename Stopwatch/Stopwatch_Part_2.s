cc79e0fd9281201105f0051d07b509eb5676f2c7
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-09-18 19:03:24 -0400

    Comment Update v1

--- Stopwatch/Stopwatch_Part_2.s
+++ Stopwatch/Stopwatch_Part_2.s
@@ -1,211 +1,253 @@
-@ to compile, use gcc -o stopwatch stopwatch.s -lc
+@ Stopwatch_Part_2
+@
+@ ECSE 4235 - Embedded Systems II
+@ Property of Samuel Brewster and Simline Gijo	
+@ Created 18SEP2024
+@
+@ To Compile, Use: gcc -o Stopwatch_Part_2 Stopwatch_Part_2.s -lc
+
+
 	.text
     .global main
     .include "./E4235_KYBdeblock.s"
 
+@ Main Function -Initializes Variabes and Calls Deblock.
 main:
 	mov r5, #0
 	mov r6, #0
-    b _Deblock
+    b _deblock
 
-_Deblock:  
-    mov r0, #1
-    bl  E4235_KYBdeblock
-    b _start
+@ Deblock Function - Calls Deblock from Class Library.
+_deblock:  
+    mov r0, #1				@ when r0 = 1, calling deblock enables it
+    bl  E4235_KYBdeblock	@ call Class Library Deblock
+    b _start				@ Begin Program
 
-_Read:
-	mov r0, #0	@ file descriptor for stdin
-	mov r2, #1	@ allocates bytes to read
-	mov r7, #3	@ svc read
-	ldr r1, =input
-	str r0, [r1]	@ reset input
+@ Read Function - Read Input from STDIN and Store in a variable in =input.
+_read:
+	mov r0, #0				@ file descriptor for stdin
+	mov r2, #1				@ allocates bytes to read
+	mov r7, #3				@ svc read
+	ldr r1, =input			@ load the address of the =input variable
+	str r0, [r1]			@ reset input
 	svc 0
-	ldrb r1, [r1]	@ read byte in input
+	ldrb r1, [r1]			@ read byte in input
 	bx lr
-       
-_Check:
-	cmp r1, #'r'    @ blocking
+  
+@ Check Function - Checks if the user input is valid and branches accordingly.  
+_check:
+	cmp r1, #'R'    		@ Run
 	beq _run
-	cmp r1, #'l'    @ deblocking
+	cmp r1, #'L'    		@ Lap
 	beq _lap
-    cmp r1, #'s'    @ quit
+    cmp r1, #'S'    		@ Stop
     beq _stop
-    cmp r1, #'c'    @ quit
+    cmp r1, #'C'    		@ Clear
     beq _clear
-    cmp r1, #'q'    @ quit
-    beq _exit
-	bx   lr
+	bx   lr					@ Return to tracking time
       
+@ Run Function - Sets the program to run and stop lapping.
 _run:
-	mov r5, #1
-	mov r6, #0
-	bx lr
+	mov r5, #1				@ Set Run Flag = 1
+	mov r6, #0				@ Set Lap Flag = 1
+	bx lr					@ Return to tracking time
       
+@ Lap Function - Makes the Program Start Lapping, aka, stop printing.
 _lap:
-	mov r6, #1		@ move a 1 into the lap register
-	bx   lr
-        
+	mov r6, #1				@ Set Lap Flag = 1
+	bx   lr					@ Return to tracking time
+  
+@ Stop Function - Stops the Program from tracking time, basically just stops the run function.  
 _stop:
-	mov r5, #0
-	bx lr
+	mov r5, #0				@ Set Run Flag = 0
+	bx lr					@ Return to tracking time
 	
+@ Clear Function - Stops the Program with the Run Flag and Sets Variables to 0.
 _clear:
-	@ clearing run, lap, stop registers
-	mov r5, #0 @run
-	
-	ldr R3, =iterations		@ Loads the Address of the Variables into R1
-    str R5, [R3]
-    str R5, [R3, #4]	
-    str R5, [R3, #8]
-    str R5, [R3, #12]
-    str R5, [R3, #16]
-    str R5, [R3, #20]
+	mov r5, #0 				@ Set Run Flag = 0
+	ldr r3, =iterations		@ Loads the Address of the Variables into r3
     
-	bx  lr
-        
+    str r5, [r3]			@ Stores 0 in the X0:00:00 Variable
+    str r5, [r3, #4]		@ Stores 0 in the 0X:00:00 Variable
+    str r5, [r3, #8]		@ Stores 0 in the 00:X0:00 Variable
+    str r5, [r3, #12]		@ Stores 0 in the 00:0X:00 Variable
+    str r5, [r3, #16]		@ Stores 0 in the 00:00:X0 Variable
+    str r5, [r3, #20]		@ Stores 0 in the 00:00:0X Variable
+    
+	bx  lr					@ Return to tracking time
+    
+@ Start Function - Main Loop that will continually run, it only leaves to h1 when the Run Flag is Set to 1.
 _start:	
-	LDR 	R4, =iterations		@ Loads address of variables
-	MOV 	R3, #0			
-	STR 	R3, [R4, #4]		@ Sets Minutes Variable to Zero
+	LDR 	r4, =iterations	@ Loads address of the variables
+	MOV 	r3, #0			
+	STR 	r3, [r4]		@ Sets Decaminutes Variable to Zero (Edge Case)
 
-l1:	LDR	R3, =8900000		@ R3 = 8,900,000 
-l2:	SUBS	R3, R3, #1		@ R3 = R3 – 1, decrement R1 
-	BNE	l2			@ repeat it until R3 = 0 
+l1:	ldr	r3, =8900000		@ r3 = 8,900,000 
+l2:	subs	r3, r3, #1		@ r3 = r3 – 1, decrement r1 
+	bne	l2					@ repeat it until r3 = 0 
 	
-	bl _Read
-	bl _Check
-	cmp r5, #1
-	BEQ _h1
-	b l1
+	bl _read				@ Poll the User
+	bl _check				@ Check User Input
+	cmp r5, #1				@ Checks if the program is running
+	beq _h1					@ Branch if so
+	b l1					@ Loop again if not so
    
-@ The _h1 function increments the centisecond variable until it reaches 10 and then branches to _h2.
+@ H1 Function - Increments the centisecond variable (00:00:0X) until it reaches 10 and then branches to _h2.
 _h1:
-        LDR 	R3, =iterations		@ Loads the Address of the Variables into R1
-        LDR 	R3, [R3, #20]		@ Loads the Centisecond Variable into R1
-        ADDS 	R3, #0x1		@ Increments R1
-        LDR 	R4, =iterations		
-        STR 	R3, [R4, #20]           @ Stores the incremented value of R1 into the Centiseconds Variable
+    ldr 	r3, =iterations		@ Loads the Address of the Variables into R1
+    ldr 	r3, [r3, #20]		@ Loads the Centisecond Variable into R1
+    adds 	r3, #0x1			@ Increments R1
+    ldr 	r4, =iterations		
+    str 	r3, [r4, #20]       @ Stores the incremented value of R1 into the Centiseconds Variable
         
-		CMP 	R3, #10			@ Compares to see if R1 and  10 are equal
-        BEQ 	_h2			@ If so, branches to _h2.
+	CMP 	r3, #10				@ Sees if it has reached 10 loops.
+    beq 	_h2					@ If so, branches to _h2.
         
-        cmp r6, #1
-		BEQ l1
-        B	print			@ Prints 
+    cmp 	r6, #1				@ Is it lapping?
+	beq 	l1					@ If so, branch and do not print.
+    b		print				@ Prints 
         
-@ The _h2 function resets the centisecond variable, increments the decisecond variable until it reaches 10, and then branches to _s1.
+@ H2 Function - Increments the decisecond variable (00:00:X0) until it reaches 6 and then branches to _s1.
 _h2:
-	MOV 	R3, #0			
-	STR 	R3, [R4, #20]		@ Stores the value Zero in the Centiseconds Variable
+	mov 	r3, #0				
+	str 	r3, [r4, #20]		@ Stores the value Zero in the Centiseconds Variable (00:00:0X)
 		
-	LDR 	R3, =iterations
-    LDR 	R3, [R3, #16]		@ Loads the Decisecond into R1
-    ADDS 	R3, #0x1
-    LDR 	R4, =iterations
-    STR 	R3, [R4, #16]           @ Stores the incremented value of R1 into the Deciseconds Variable
+	ldr	    r3, =iterations			
+    ldr 	r3, [r3, #16]		@ Loads the Value in the Decisecond Variable into r3
+    adds 	r3, #0x1
+    ldr 	r4, =iterations
+    str 	r3, [r4, #16]       @ Stores the incremented value of r3 into the Deciseconds Variable
         
-	CMP 	R3, #10
-    BEQ 	_s1
+	cmp 	r3, #10
+    beq 	_s1
         
-	cmp r6, #1
-	BEQ l1
-	B	print       
+	cmp 	r6, #1				@ Is it lapping?
+	beq 	l1					@ If so, branch and do not print.
+    b		print				@ Prints 
 	
 	
 @ The _s1 function resets the decisecond variable, increments the second variable until it reaches 10, and then branches to _s2.
 _s1:
 
-	MOV 	R3, #0
-	STR 	R3, [R4, #16]		@ Stores the value Zero in the Deciseconds Variable
+	mov 	r3, #0
+	str 	r3, [r4, #16]		@ Stores the value Zero in the Deciseconds Variable
 	
-	LDR 	R3, =iterations
-        LDR 	R3, [R3, #12]		@ Loads the Second into R1
-        ADDS 	R3, #0x1			
-        LDR 	R4, =iterations
-        STR 	R3, [R4, #12]            @ Stores the incremented value of R1 into the Seconds Variable
+	ldr 	r3, =iterations
+    ldr 	r3, [r3, #12]		@ Loads the Second into R1
+    adds 	r3, #0x1			
+    ldr 	r4, =iterations
+    str 	r3, [r4, #12]       @ Stores the incremented value of r3 into the Seconds Variable
         
-	CMP 	R3, #10
-        BEQ 	_s2
+	cmp 	r3, #10
+    beq 	_s2
         
-	cmp r6, #1
-	BEQ l1
-	B	print  
+	cmp 	r6, #1				@ Is it lapping?
+	beq 	l1					@ If so, branch and do not print.
+    b		print				@ Prints 
 
 @ The _s2 function resets the second variable, increments the decasecond variable until it reaches 10, and then branches to _m1.
 _s2:
-	MOV 	R3, #0
-	STR 	R3, [R4, #12]		@ Stores the value Zero in the Seconds Variable
+	mov 	r3, #0
+	str 	r3, [r4, #12]		@ Stores the value Zero in the Seconds Variable
 	
-	LDR 	R3, =iterations
-        LDR 	R3, [R3, #8]		@ Loads the Decasecond into R1
-        ADDS 	R3, #0x1			
-        LDR 	R4, =iterations
-        STR 	R3, [R4, #8]            @ Stores the incremented value of R1 into the Decaseconds Variable
+	ldr 	r3, =iterations
+    ldr 	r3, [r3, #8]		@ Loads the Decasecond into R1
+    adds 	r3, #0x1			
+    ldr 	r4, =iterations
+    str 	r3, [r4, #8]        @ Stores the incremented value of R1 into the Decaseconds Variable
         
-	CMP 	R3, #6
-        BEQ 	_m1
+	cmp 	r3, #6
+    beq 	_m1
         
-	cmp r6, #1
-	BEQ l1
-	B	print  
+	cmp 	r6, #1				@ Is it lapping?
+	beq 	l1					@ If so, branch and do not print.
+    b		print				@ Prints 
 
 @ The _m1 function resets the decasecond variable, increments the hectosecond variable until it reaches 10, and then branches to _start.
 _m1:
-	MOV 	R3, #0
-	STR 	R3, [R4, #8]		@ Stores the value Zero in the Decaseconds Variable  
+	mov 	r3, #0
+	str 	r3, [r4, #8]		@ Stores the value Zero in the Decaseconds Variable  
 	
-	cmp r6, #1
-	BEQ l1
-	B	print  
+	ldr 	r1, =iterations
+    ldr 	r1, [r1, #4]		@ Loads the minutes variable into R1
+    adds 	r1, #0x1			
+    ldr 	r2, =iterations
+    str 	r1, [r2, #4]        @ Stores the incremented value of R1 into the Hectoseconds Variable
+        
+	cmp 	r1, #10	 
+	beq 	_m2
+	
+	cmp 	r6, #1				@ Is it lapping?
+	beq 	l1					@ If so, branch and do not print.
+    b		print				@ Prints 
+
+@ The _m2 function resets the minutes variable, increments the decaminutes variable until it reaches 6, and then branches to _start.
+_m2:
+	mov 	r3, #0
+	str 	r3, [r4, #4]		@ Stores the value Zero in the Minutes Variable  
+	
+	ldr 	r1, =iterations
+    ldr 	r1, [r1]			@ Loads the decaminutes variable into R1
+    adds 	r1, #0x1			
+    ldr 	r2, =iterations
+    str 	r1, [r2, #4]        @ Stores the incremented value of R1 into the Hectoseconds Variable
+        
+	cmp 	r1, #6	 
+	beq 	_start
+	
+	cmp 	r6, #1				@ Is it lapping?
+	beq 	l1					@ If so, branch and do not print.
+    b		print				@ Prints 
+
 
 print: 
 @ print time
 
-	LDR 	R0, =string         	@ Load the String Format Location in R0
-        LDR 	R1, =iterations		@ Load Address of the Variables into R1
-        LDR 	R1, [R1]            	@ Load the Decaminutes variable
-        BL 	printf			@ Print and wipe the Registers
+	ldr 	r0, =string         	@ Load the String Format Location in R0
+    ldr 	r1, =iterations		@ Load Address of the Variables into R1
+    ldr 	r1, [r1]            	@ Load the Decaminutes variable
+    bl 	    printf			@ Print and wipe the Registers
 	
-	LDR 	R0, =string         	
-        LDR 	R1, =iterations		
-        LDR 	R1, [R1, #4]        	@ Load the minutes variable
-        BL 	printf
+	ldr 	r0, =string         	
+    ldr 	r1, =iterations		
+    ldr 	r1, [r1, #4]        	@ Load the minutes variable
+    bl 	    printf
 
-	LDR 	R0, =colon          	@ Load the Colon String in R0
-        BL 	printf
+	ldr 	r0, =colon          	@ Load the Colon String in R0
+    bl 	    printf
 
-	LDR 	R0, =string         	
-        LDR 	R1, =iterations		
-        LDR 	R1, [R1, #8]            @ Load the deaseconds variable
-        BL 	printf
+	ldr 	r0, =string         	
+    ldr 	r1, =iterations		
+    ldr 	r1, [r1, #8]            @ Load the deaseconds variable
+    bl   	printf
 	
-	LDR 	R0, =string         	
-        LDR 	R1, =iterations		
-        LDR 	R1, [R1, #12]           @ Load the seconds variable
-        BL 	printf
+	ldr 	r0, =string         	
+    ldr 	r1, =iterations		
+    ldr 	r1, [r1, #12]           @ Load the seconds variable
+    bl 	    printf
 	
-	LDR 	R0, =colon         	@ Load the Colon String in R0
-        BL 	printf
+	ldr 	r0, =colon         	@ Load the Colon String in R0
+    bl 	    printf
 	
-	LDR 	R0, =string         	
-        LDR 	R1, =iterations		
-        LDR 	R1, [R1, #16]           @ Load the deciseconds variable
-        BL 	printf
+	ldr 	r0, =string         	
+    ldr 	r1, =iterations		
+    ldr 	r1, [r1, #16]           @ Load the deciseconds variable
+    bl 	    printf
 
-    LDR 	R0, =string         	
-    LDR 	R1, =iterations		
-    LDR 	R1, [R1, #20]           @ Load the centiseconds variable
-    BL 	printf
+    ldr 	r0, =string         	
+    ldr 	r1, =iterations		
+    ldr 	r1, [r1, #20]           @ Load the centiseconds variable
+    bl   	printf
     
-	LDR 	R0, =newline         	@ Load the newline character in R0
-    BL 	printf
+	ldr 	r0, =newline         	@ Load the newline character in R0
+    bl 	    printf
     
-	B 	l1
+	b 	l1
 
 
 @ Used to exit but will never be reached	
 _exit:
-        mov R7, #1
+        mov r7, #1
         svc 0
 
 
@@ -219,7 +261,6 @@ newline:
 	.asciz "\n"			@ Printable New Line Character
 iterations:
         .word 0, 0, 0, 0, 0, 0  	@ Variables for Clock in order of: MM:SS:HH
-output: .ascii "xxxx\n"
 inputform:
         .asciz "%d"
 input:
