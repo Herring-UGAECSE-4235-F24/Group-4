bc4009233234dcb0f82c46fa78d00ceafd20d05d
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-09-18 12:32:08 -0400

    properly reads input while deblocked!

--- Stopwatch/test2.s
+++ Stopwatch/test2.s
@@ -9,22 +9,44 @@ main:
 _Deblock:  
     mov r0, #1
     bl  E4235_KYBdeblock
-    b _Scan
+    b _Read
     
 _Scan:
-	ldr r0, = inputform
+	ldr r0, =inputform
     ldr r1, =input
     bl scanf
     ldr r1, =input
-
     ldr r1, [r1]
 
+_Read:
+	mov r0, #0	@ file descriptor for stdin
+	mov r2, #1	@ allocates bytes to read
+	mov r7, #3	@ svc read
+	ldr r1, =input
+	str r0, [r1]	@ reset input
+	svc 0
+	ldrb r1, [r1]	@ read byte in input
+
+_Check:
+	cmp r1, #'b'    @ blocking
+	beq _Print
+	cmp r1, #'d'    @ deblocking
+	beq _Print
+    cmp r1, #'q'    @ quit
+    beq _Print
+	b   _Read
+
 _Print:
-	mov r0, #1 @Std Out
-    ldr r1, =output
-    mov r2, #5 @length of out string
-    mov r7, #4 @write sys call
-    svc 0
+	ldr r0, =string @Std Out
+	bl printf
+	
+	LDR 	R0, =newline         	@ Load the newline character in R0
+    BL 	printf
+	
+    @ldr r1, =output
+    @mov r2, #1 @length of out string
+    @mov r7, #4 @write sys call
+    @svc 0
     b _Scan
 
 @ Used to exit but will never be reached	
