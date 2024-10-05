392f2f1291a677ebe9fbcd1d8a7c19f9496d1419
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-10-04 23:50:58 -0400

    Push 12 - Simline 2

--- State_Machine/State_Machine.c
+++ State_Machine/State_Machine.c
@@ -9,165 +9,62 @@
 // Revised the makefile to resolve the compilation and linking executable issues. Now, the keypad doesn't work anymore.
 // Seemed to work again after using the wrong compilation command and then recompiling it correctly
 
+// 1 Khz in class library is 1,000,000 iterations total, 500,000 on/off
+// By testing, 1 kHz is about 860,000 iterations total or 430,000 on/off.
+
 #include "stdio.h"
 #include "gpiotopin.h"
-//#include "E4235.h"
+#include <bcm2835.h>
 
-//extern int E4235_Read(int); // GPIO
-//extern int E4235_Write(int, int); // GPIO,value (on/off)
-//extern void E4235_Delaynano(int); // time
+extern int E4235_Write(int, int); // GPIO,value (on/off)
+extern void E4235_Delaynano(int); // time
 
-// dec to char: 
-// 49 -> 1, 50 -> 2, 51 -> 3, 65 -> 4
-// 42 -> *, 48 -> 0, 35 -> #, 68 -> D
-
-
-int readKeyPad(GPIONumber){
-	
-	int rowPushed = 0;
-		
-	E4235_Write(GPIONumber, 1);
-	
-	if (E4235_Read(GPIO24) == 1){
-		rowPushed = 1;
-		printf("1st row\n");
-	}
-	if (E4235_Read(GPIO25) == 1){
-		rowPushed = 2;
-		printf("2nd row\n");
-	}
-	if (E4235_Read(GPIO26) == 1){
-		rowPushed = 3;
-		printf("3rd row\n");
-	}
-	if (E4235_Read(GPIO27) == 1){
-		rowPushed = 4;
-		printf("4th row\n");
-	}
-	
-	E4235_Write(GPIONumber, 0); // may need to move this down to translateNumber() as the last line
-	
-	return rowPushed;
-	
-} // readKeyPad
-
-int translateNumber(GPIONumber, selectedNumber) {
-	
-	if (GPIONumber == 20) {
-		if (selectedNumber == 1) {
-			printf("1 dumbo\n");
-			return '1';
-		} else if (selectedNumber == 2) {
-			printf("2 dumbo\n");
-			return '2';
-		} else if (selectedNumber == 3) {
-			printf("3 dumbo\n");
-			return '3';
-		} else if (selectedNumber == 4) {
-			printf("4 dumbo\n");
-			return 'A';
-		}
-	 } 
-	 
-	 if (GPIONumber == 21) {
-		if (selectedNumber == 1) {
-			printf("4 dumbo\n");
-			return '4';
-		} else if (selectedNumber == 2) {
-			printf("5 dumbo\n");
-			return '5';
-		} else if (selectedNumber == 3) {
-			printf("6 dumbo\n");
-			return '6';
-		} else if (selectedNumber == 4) {
-			printf("B dumbo\n");
-			return 'B';
-		}
-	 }
-	
-	if (GPIONumber == 22) {
-		if (selectedNumber == 1) {
-			printf("7 dumbo\n");
-			return '7';
-		} else if (selectedNumber == 2) {
-			printf("8 dumbo\n");
-			return '8';
-		} else if (selectedNumber == 3) {
-			printf("9 dumbo\n");
-			return '9';
-		} else if (selectedNumber == 4) {
-			printf("C dumbo\n");
-			return 'C';
-		}
-	 }
-	 
-	 if (GPIONumber == 23) {
-		if (selectedNumber == 1) {
-			printf("* dumbo\n");
-			return '*';
-		} else if (selectedNumber == 2) {
-			printf("0 dumbo\n");
-			return '0';
-		} else if (selectedNumber == 3) {
-			printf("# dumbo\n");
-			return '#';
-		} else if (selectedNumber == 4) {
-			printf("D dumbo\n");
-			return 'D';
-		}
-	 }
-	//E4235_Write(GPIONumber, 0);
-	return 'L';
+int bintohex()
+{
+	int len = str(binary);
 }
 
-int lookForInput(){
-	
-	int stop = 0;
-	int yes = 0;
-		
-	stop = readKeyPad(20); // may need to move E4235_Write(GPIONumber, 0); to right after this line
-	if (stop != 0) {
-		return translateNumber(20, stop); // this may be returning a character instead of an int
-	}
-	stop = readKeyPad(21);
-	if (stop != 0) {
-		yes = translateNumber(21, stop);
-		printf("GPIO21:\n", yes);
-		return yes;
-	}
-	stop = readKeyPad(22);
-	if (stop != 0) {
-		return translateNumber(22, stop);
-	}
-	stop = readKeyPad(23);
-	if (stop != 0) {
-		yes = translateNumber(23, stop);
-		printf("23!!!\n", yes);
-		return yes;
-	}
-				
-	return stop;
-			
-} // Looking For Input
 
-
+// Main Loop
 int main(int argc, char **argv) {
 	
-	char selectedNumber = 0; // original was int
+	int isOn = 0;
+	char binar[100];
+	int num = 40;
 	
-	while (1) {
-		
-		selectedNumber = 0; 
-		
-		while (selectedNumber == 0) {
-			selectedNumber = lookForInput(); 
-		}
-		
-		if (selectedNumber != 0) {
+	//power up and displaying hex 40 (while assuming decode is off) and logic analyzer should display hex 40
+	
+	// waiting for keypress
+	if (E4235_Write(20, 1) && (bcm2835_gpio_lev(GPIO24) == 1)){
+		rowPushed = 1;
+	}
+	
+	//replace who translateNumber() with my hex printf line
+	
+	while(1) {
+		if (isOn == 0) {
 			
-			printf("Output: %c\n", selectedNumber); //original was decimal
+			E4235_Write(9, 1);
+			E4235_Delaynano(430000);
+			isOn = 1;
+		
+		} else {
+		
+			E4235_Write(9, 0);
+			E4235_Delaynano(430000);
+			isOn = 0;
+		}
+
+	} // while
+	
+	while(keypress == 1)
+	{
+		if (E4235_Write(20, 1) && (bcm2835_gpio_lev(GPIO24) == 1)){
+			//output of logic analyzer gets converted to hex here
+			printf("Output: %X", num);
 		}
 	}
 				
 	return 0;
+	
 } // Main
