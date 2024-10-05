fb5db22d4540c66e7e24f96dbb2311fc77eb10a2
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-10-04 23:43:28 -0400

    Push 4

--- State_Machine/State_Machine.c
+++ State_Machine/State_Machine.c
@@ -17,6 +17,48 @@ extern int E4235_Read(int); // GPIO
 extern int E4235_Write(int, int); // GPIO,value (on/off)
 extern void E4235_Delaynano(int); // time
 
+int readKeyPad(gpioNumber){
+	
+	int rowPushed = 0;
+		
+	E4235_Write(gpioNumber, 1);
+	
+	if (E4235_Read(GPIO24) == 1){
+		rowPushed = 1;
+	}
+	if (E4235_Read(GPIO25) == 1){
+		rowPushed = 2;
+	}
+	if (E4235_Read(GPIO26) == 1){
+		rowPushed = 3;
+	}
+	if (E4235_Read(GPIO27) == 1){
+		rowPushed = 4;
+	}
+	
+	E4235_Write(gpioNumber, 0);
+	
+	return rowPushed;
+	
+} // readKeyPad
+
+ translateNumber(GPIONumber, selectedNumber) {
+	
+	if (GPIONumber == 20) {
+		if (selectedNumber == 1) {
+			return '1';
+		} else if (selectedNumber == 2) {
+			return '2';
+		} else if (selectedNumber == 3) {
+			return '3';
+		} else if (selectedNumber == 4) {
+			return 'A';
+		}
+	}
+	
+	return 'L';
+}
+
 int main(int argc, char **argv) {
 	
 	int pin20 = 0;
@@ -28,35 +70,28 @@ int main(int argc, char **argv) {
 	int pin26 = 0;
 	int pin27 = 0;
 	int stop = 1;
+	int selectedNumber;
+	int returnedChar;
+
+	char rowOne [3];
+	rowOne [0] = '1';
+	rowOne [1] = '2';
+	rowOne [2] = '3';
+	rowOne [3] = 'A';
 	
 	while(stop != 0) {
 		
-		pin24 = readKeyPad(GPIO20);
-		printf("24: %d\n", pin24);
+		selectedNumber = 0;
+		returnedChar = '0';
+		
+		selectedNumber = readKeyPad(GPIO20, rowOne);
+		if (selectedNumber != 0) {
+			returnedChar = translateNumber(GPIO20, selectedNumber);
+		}
+		
+		printf("24: %c\n", returnedChar);
+		
 	}
 				
 	return 0;
 } // Main
-
-
-
-int readKeyPad(gpioNumber){
-		
-	E4235_Write(gpioNumber, 1);
-	
-	if (E4235_Read(GPIO24) == 1){
-		return 1;
-	}
-	if (E4235_Read(GPIO25) == 1){
-		return 2;
-	}
-	if (E4235_Read(GPIO26) == 1){
-		return 3;
-	}
-	if (E4235_Read(GPIO27) == 1){
-		return 4;
-	}
-	
-	return 0;
-	
-} // readKeyPad
