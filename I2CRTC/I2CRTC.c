ddfc776e43f7413e8593e53545aabb53ea663903
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-10-23 12:26:49 -0400

    Some Good Ideas in how to change between read/write mode

--- I2CRTC/I2CRTC.c
+++ I2CRTC/I2CRTC.c
@@ -1,4 +1,4 @@
-\# include <stdio.h>
+# include <stdio.h>
 # include <bcm2835.h>
 
 # define MODE_READ 1
@@ -16,9 +16,9 @@ void initialize() {
 		return;
     }
 	
-	//bcm2835_i2c_setSlaveAddress(0x68);
+
 	//bcm2835_i2c_setClockDivider(32000);
-	//bcm2835_i2c_set_baudrate(100000);
+	bcm2835_i2c_set_baudrate(100000);
 	
 	return;
 }
@@ -33,16 +33,36 @@ int main(int argc, char **argv) {
 	
 	initialize();
 
-	//uint8_t data;
+	char zeroBuf[8];
+	char oneBuf[8];
+	char testBuf[8];
 	
-	//printf("before seg fault");
-	//bcm2835_i2c_setSlaveAddress(0x68);
-	//printf("before seg fault");
-	//data = bcm2835_i2c_write(0x00, 1);
+	char one[1];
+	one[0] = 1;
+	
+	char zero[1];
+	zero[0] = 0;
+
+	for (int i = 0; i < 8; i++) {
+		zeroBuf[i] = 0;
+	}
+	for (int i = 0; i < 8; i++) {
+		oneBuf[i] = 1;
+	}
+	//0x68? -> 01101000 -> 1101000X -> (C1) (C0)
+	bcm2835_i2c_setSlaveAddress(0x68);
+	bcm2835_i2c_write(zero, 1); // Write 0 to start writing process to i2c, second arg now works with a char array
+	
+	for (int i = 0; i < 1; i++) {
+		testBuf[i] = 1;
+	}
+	bcm2835_i2c_write(testBuf, 1); // Write 0 to start writing process to i2c, for some reason, second arg has to be 0?
+	bcm2835_i2c_setSlaveAddress(0x68);
+	bcm2835_i2c_write(one, 1); // Write 0 to start writing process to i2c, second arg now works with a char array
 	
 	// seg faulting here
-    //data = bcm2835_i2c_read(buf, len);
-    //printf("Read Result = %d\n", data);   
+	bcm2835_i2c_read(testBuf, 1);
+    printf("Read Result = %d\n", testBuf); 
     
 	bcm2835_i2c_end();  
 	bcm2835_close();
