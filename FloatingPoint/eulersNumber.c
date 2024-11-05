d9a40f116e066fe78e1ccbba96693c2beba35e3f
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-11-05 17:50:12 -0500

    Initial Push

---
+++ FloatingPoint/eulersNumber.c
@@ -0,0 +1,12 @@
+// eulersNumber.c
+// Property of Group 4 - Sam Brewster and Simline Gijo
+// Push - Initial Push
+
+#include <stdio.h>
+
+// Main Loop
+int main(int argc, char **argv) {
+
+  return 0;
+  
+} // main
