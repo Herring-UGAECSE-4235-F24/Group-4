b1ce3d25dad738cac895ec3edb64433c61a4e9f1
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-11-06 17:47:56 -0500

    Scan F Works

--- FloatingPoint/nfactorial.s
+++ FloatingPoint/nfactorial.s
@@ -1,7 +1,7 @@
 @ nfactorial.s
 @ Property of Group 4
 @
-@ Push Name: Not Properly Reading User Input - Lets try Scanf
+@ Push Name: Scan F Works
 @ 
 @ The Idea is to print the input line, read the input, then convert to a float and do the operations, then print the output line.
 @ Factorial Logic: CMP if variable is 0, if so, load 1 and branch to exit. 
@@ -14,28 +14,17 @@ main:
 	push {r0-r4, lr}
 	ldr 	r0, =inputline  @ Load the Print Input String Location in R0
 	bl 	printf	  	@ Print and wipe the Registers
-	b _scan
 
-_read:
-	mov r0, #0		@ Input the file descriptor for STDIN
-	mov r2, #1		@ Allocates the data to be read as 1 byte
-	mov r7, #3		@ Load the svc code for read
-	ldr r1, =inputINT		@ Load the address of the =input variable
-	str r0, [r1]		@ Clear the Input Variable (make it equal 0)
-	svc 0             	@ Call the System Call
-
-_scan:
-	ldr r1, =inputINT		@ Load the address of the =input variable
+	ldr r0, =format
+	ldr r1, =inputINT
 	bl scanf
 
 _initLogic: 
-  	vldr.f32 s15, [r1]	@ Read the data stored in the input variable into r1 
-s1:
- 	vcvt.f64.f32 d7, s15 		@ Convert to a double
-s2:
+	
 	ldr 	r0, =outputline    	@ Load the String Format Location in R0
-	vmov 	r2, r3, d7		@ Mov to be printed
-s3:
+	ldr r1, =inputINT		@ Load the address of the =input variable
+	ldr r1, [r1]
+s1:
 	bl 	printf			@ Print and wipe the Registers
 
 _exit:
@@ -46,9 +35,9 @@ _exit:
 .data
 inputINT:
     .word 0
-outputFLOAT:
-    .float 0
 inputline:
     .string "Enter n:"
+format:
+	.string "%i"
 outputline:
-    .string "\nn! = %f\n"
+    .string "\nn! = %i\n"
