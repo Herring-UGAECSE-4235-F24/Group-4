0a96d15cc505f74760855382204bc393e7191b26
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-11-06 11:52:48 -0500

    After Using GDB to mess with Printing

--- FloatingPoint/fpEx1.s
+++ FloatingPoint/fpEx1.s
@@ -1,6 +1,6 @@
 @ fpEx1.s 
 @ Property of Group 4 - Sam Brewster and Simline Gijo
-@ Push: Logic Works on Paper - Test Later
+@ Push: After Using GDB to mess with Printing
 @
 @ Group assignment
 @ This program calculates the area of a circle
@@ -15,19 +15,22 @@
 main:
 	vmov.f32	s0,#0.125		
 	vmul.f32 	s0, s0, s0
-	ldr		r2,=piNumber
-	vldr		s1, [r2]
+	ldr			r2,=piNumber
+	vldr.f32	s1, [r2]
 	vmul.f32	s0, s0, s1
 
 _printConvert:
-  	ldr 		r0, =outputline    	@ Load the String Format Location in R0
+  	ldr 			r0, =outputline    	@ Load the String Format Location in R0
+s1:
  	vcvt.f64.f32 	d0, s0 			@ Convert to a double
-  	vmov 		r1, r2, d0		@ Move to be printed
-    	bl 		printf			@ Print and wipe the Registers
+s2:
+  	vmov.f32 		r0, r1, d0		@ Move to be printed
+s3:
+    bl 				printf			@ Print and wipe the Registers
 
 	
 _infiniteLoop:	
-	b	_inifiteLoop
+	@b	_infiniteLoop
 
  _exit:
 	mov r7, #1		@ Load the Exit Value
@@ -36,8 +39,6 @@ _infiniteLoop:
 .data
 piNumber: 
 	.float 3.141593
-inputline:
-    	.asciz "Enter n:"
 outputline:
-    	.asciz "n! = %f"
+    	.asciz "Area = %d\n"
 	
