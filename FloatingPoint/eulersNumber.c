3ee398c08d70b359e913b6655a1ecba0f09ec8e2
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-11-07 00:15:11 -0500

    Can Read in Both Assembly Files

--- FloatingPoint/eulersNumber.c
+++ FloatingPoint/eulersNumber.c
@@ -1,6 +1,6 @@
 /* eulersNumber.c
  Property of Group 4 - Sam Brewster and Simline Gijo
- Push: Change from Float to Double - Printf cant print Floats
+ Push: Can Read in Both Assembly Files
  
 3) Now, lets use your two functions to calculate a e^x using series expansion
 Write a C program that calls your two assembly functions to calculte the e^x (0 and positive reals are okay) out to the first 5 terms of the Taylor series
@@ -17,23 +17,23 @@ Helpful Notes - https://stackoverflow.com/questions/13901261/calling-assembly-fu
  
 #include <stdio.h>
 
-extern "C" {double nfactorial(int n);}
-extern "C" {double nexponential(int n, int x);}
+extern float nfactorial(float n);
+extern float nexponential(float n, float x);
 
 // Main Loop
 int main(int argc, char **argv) {
 
   // Declare Variables
-  double tempNF;
+  float tempNF;
   tempNF = 0;
-  double tempNE;
+  float tempNE;
   tempNE = 0;
-  double x;
-  double eSum; 
+  float x;
+  float eSum; 
 
   // Initial User Interaction
   printf("Calculate e^x.  Enter x:");
-  x = scanf();
+  scanf("%f", &x);
 
   eSum = 1;   // + 1
 
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
   for (int i = 2; i < 6; i++) {
 
       tempNF = nfactorial(i);        // Get NFactorial Based on the Loop Iteration
-      tempNE = nexponential(n, i);   // Get x^n Based on the Loop Iteration
+      tempNE = nexponential(x, i);   // Get x^n Based on the Loop Iteration
       eSum += tempNE/tempNF;         // Add them and increment through until n = 6
     
   }
