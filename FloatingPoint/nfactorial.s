9f301cd76953e6ed52f365a9dcd1c55028d8ebe8
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-11-06 22:44:11 -0500

    IT WORKS!

--- FloatingPoint/nfactorial.s
+++ FloatingPoint/nfactorial.s
@@ -1,7 +1,7 @@
 @ nfactorial.s
 @ Property of Group 4
 @
-@ Push Name: Literally JUST VCMP WONT WORK
+@ Push Name: IT WORKS!
 
 .text
 .global main
@@ -11,16 +11,39 @@ main:
 	ldr 	r0, =inputline  @ Load the Print Input String Location in R0
 	bl 	printf	  	@ Print and wipe the Registers
 
-	ldr r0, =format			@ Prepare what to read
-	ldr r1, =inputINT		@ Prepare where to store it
-	bl scanf				@ Poll the User
+	ldr r0, =format		@ Prepare what to read
+	ldr r1, =inputINT	@ Prepare where to store it
+	bl scanf		@ Poll the User
 
 _initLogic: 
+	ldr r1, =inputINT	@ Load the address of the =input variable
+	vldr.f32 s15, [r1]	@ Read the data stored in the input variable into r1 
 	
-	ldr r0, =outputline    	@ Load the String Format Location in R0
-	ldr r1, =inputINT		@ Load the address of the =input variable
+ 	vmov.f32  s14, s15      @ Create a duplicate of s15
 	
-	vldr.f32 s15, [r1]	 	@ Read the data stored in the input variable into r1 
+	ldr r2, =floatOne
+	vldr.f32  s13, [r2]	@ Fill a register with 1
+	
+	ldr r2, =floatZero
+	vldr.f32  s12, [r2]	@ Fill a register with 0
+	
+_topOfLoop:
+  	vsub.f32  s14, s14, s13 @ Subtract 1 from the counter
+   	vcmp.f32  s14, #0x00000000	@ THIS IS THE ONLY LINE NOT WORKING RN IM SO MAD
+	vmrs       r10, FPSCR
+	and	   r11, r10, #0x40000000
+	cmp        r11, #0x40000000
+  	beq        _storeVar 	@ Exit if s14 equals 0
+  	vmul.f32  s15, s15, s14 @ if s14 != 0 then s15 = s15 * (s15 - 1)
+ 	b         _topOfLoop  	@ Back to the top of the loop
+
+_storeVar:
+	ldr 	  r0, =outputline @ Load the String Format Location in R0
+  	ldr   	  r1, =inputINT  	@ Store the address of the float variable
+  	vstr.f32  s15, [r1]		@ Store the Current Value in the variable
+	
+_print:
+ 	vldr.f32 s15, [r1]	 	@ Read the data stored in the input variable into r1 
  	vcvt.f64.f32 d7, s15 	@ Convert to a double
 	ldr 	r0, =outputline @ Load the String Format Location in R0
 
@@ -33,11 +56,15 @@ _exit:
  	svc 0			@ Exit the Program
 
 .data
+floatZero:
+	.float 0.000
+floatOne:
+	.float 1.000
 inputINT:
-    .float 0
+	.float 0.000
 inputline:
-    .string "Enter n: "
+	.string "Enter n: "
 format:
 	.string "%f"
 outputline:
-    .string "You typed in: %f\n"
+	.string "You typed in: %f\n"
