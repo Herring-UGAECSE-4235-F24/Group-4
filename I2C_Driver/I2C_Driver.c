cff24c0490be5d6da01467867227fceb248794be
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-11-14 11:57:21 -0500

    Intial Push - What Ideas we have

---
+++ I2C_Driver/I2C_Driver.c
@@ -0,0 +1,15 @@
+/* I2C_Driver.c
+ * Property of Sam Brewster and Simline Gijo
+ */
+
+// We will enable the driver to drive a low and disable it to drive a high
+
+// Function to define and setup the Two GPIOs that will be used or SDA and SCL
+
+// Function to write with address and data
+
+// Function to Read address with returned data
+
+// User should be prompted to either be able to a) read the value of the rtc, b) write time to the RTC
+
+// We will most likely have to pull the GPIO high by setting the GPIO to an input and set it low by making it an output
